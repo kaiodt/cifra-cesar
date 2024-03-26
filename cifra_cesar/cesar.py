7fd698c842bb3dba6d4a9b231bde7d2f9f7ca8f5
Kaio Rocha <kaiodtr@gmail.com>
2024-03-26 09:49:28 -0300

    style: formatação do código usando o blue

--- cifra_cesar/cesar.py
+++ cifra_cesar/cesar.py
@@ -1,26 +1,31 @@
 from string import ascii_lowercase
-def encripta(frase,n=13):
+
+
+def encripta(frase, n=13):
     """encripta o texto"""
-    encriptado = ""
+    encriptado = ''
     for l in frase:
         l = l.lower()
         if l == ' ':
             encriptado += l
-        elif l not in ascii_lowercase: ...
+        elif l not in ascii_lowercase:
+            ...
         else:
             pos = ascii_lowercase.find(l) + n
             l = ascii_lowercase[pos % 26]
             encriptado += l
     return encriptado
 
-def decripta(frase,n=13):
-    "decripta o texto"
-    decriptado = ""
+
+def decripta(frase, n=13):
+    """decripta o texto"""
+    decriptado = ''
     for l in frase:
         l = l.lower()
         if l == ' ':
             decriptado += l
-        elif l not in ascii_lowercase: ...
+        elif l not in ascii_lowercase:
+            ...
         else:
             pos = ascii_lowercase.find(l) - n
             l = ascii_lowercase[pos % 26]
--- tests/test_encripta.py
+++ tests/test_encripta.py
@@ -1,13 +1,17 @@
 from cifra_cesar.cesar import encripta
 
+
 def test_encripta_kaio_deve_retornar_xnvb():
     assert encripta('Kaio') == 'xnvb'
+
 
 def test_encripta_xnvb_deve_retornar_kaio():
     assert encripta('xnvb') == 'kaio'
 
+
 def test_encripta_deve_retornar_minusculas():
     assert encripta('A').islower()
+
 
 def test_encripta_deve_preservar_espacos():
     assert encripta('A B')[1] == ' '
